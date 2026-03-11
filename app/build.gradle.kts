a19a68267b4c549946af5cbaff8aa9ec349d1d49
jupiter-rising-experiment <cecileyb.school@gmail.com>
2026-03-10 21:16:59 -0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
 
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
